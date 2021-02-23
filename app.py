2e18f3989e9b8e50bdfafa4fa12b6a216af454d8
Titus <jgengo@student.42.fr>
2021-02-23 17:24:32 +0200

    add click to app.py and move the functions

--- app.py
+++ app.py
@@ -1,56 +1,32 @@
-from tools.intra import ic
-from datetime import date
-from dateutil.relativedelta import relativedelta
-import yaml
+#!/usr/bin/env python3 -B
+
 import os
+import click
 
-with open('config.yml', 'r') as cfg_stream:
-    config = yaml.load(cfg_stream, Loader=yaml.BaseLoader)
-
-inactive_duration_month = int(config["inactive_duration_month"])
-
-
-def _get(payload):
-    data = ic.pages_threaded("cursus_users", params=payload)
-    return [i['user']['login'] for i in data]
+from tools.helpers import get_students, get_homes, delete_homes
+from loguru import logger
 
 
-def get_inactive():
-    since = (
-        date.today() - relativedelta(months=inactive_duration_month)
-    ).strftime("%Y-%m-%dT%H:%M:%S")
-    interval = since + ',' + date.today().strftime("%Y-%m-%dT%H:%M:%S")
+logger.add("logs/logs.log", rotation="500 MB", retention="10 days", compression="zip") 
 
-    payload = {
-        'filter[campus_id]' : config['campus_id'],
-        'filter[active]' : 'false',
-        'range[end_at]' : interval
-    }
-    return _get(payload)
 
-def get_active():
-    payload = {
-        'filter[campus_id]' : config['campus_id'],
-        'filter[active]' : 'true',
-    }
+@click.command()
+@click.option('--perform', is_flag=True, default=False)
+def cli(perform):
+    students_list = get_students()
+    homes_list = get_homes()
+    to_delete = list(set(homes_list) - set(students_list))
+
+    print(f"Information:\n\t- active students found: {len(students_list)}")
+    print(f"\t- homes image found: {len(homes_list)}")
+    print(f"\t- homes to delete: {len(to_delete)}")
+    print(f"\t- homes to delete: {', '.join(to_delete)}")
+
+    if perform:
+        delete_homes(to_delete)
+    else:
+        print("Run with the option --perform to delete those images")
+
     
-    return _get(payload)
-
-def main():
-    students = list(
-        set(get_active() + get_inactive() + config['allowed_list'])
-    )
-
-    files = os.listdir(config['home_dir'])
-    homes = [file.replace('.img', '') for file in files if file.endswith(".img")]
-
-    print(list(set(homes) - set(students)))
-    # students = ', '.join(map(str, students))
-    # # with open("students.txt", 'w') as f:
-    # #     f.write(students)
-    # #     f.close()
-    # return list(students)
-
-
 if __name__ == "__main__":
-    main()
+    cli()
